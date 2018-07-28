8bef800defa87aaee944153e6360600a3f8ac10c
blackchakram <blackchakram@gmail.com>
2018-07-28 12:31:47 -0600

    fine tuning to homepage mobile view.

--- js/dropdown.js
+++ js/dropdown.js
@@ -6,6 +6,13 @@ var $hamburgericon = $('.hamburger').find('img');
 var $thedropdown = $('.dropdownholder');
 var $linkblocks = $('.linkblock');
 
+var $wholink = $('.wholink');
+var $classlink = $('.classlink');
+var $testimlink = $('.testimlink');
+var $contactlink = $('.contactlink');
+
+var $offset;
+
 $hamburgericon.hover(function(){
   $thedropdown.css("display", "block");
   $thedropdown.css("z-index", "10");
@@ -23,4 +30,31 @@ $linkblocks.hover(function(){
   $(this).find('a').css("color", "white");
 }, function(){
   $(this).find('a').css("color", "black");
+});
+
+
+
+
+$wholink.click(function(){
+  $('html, body').animate({
+      scrollTop: $('.whoweare').offset().top
+  }, 500);
+});
+
+$classlink.click(function(){
+  $('html, body').animate({
+      scrollTop: $('.classes').offset().top
+  }, 500);
+});
+
+$testimlink.click(function(){
+  $('html, body').animate({
+      scrollTop: $('.testimonials').offset().top
+  }, 500);
+});
+
+$contactlink.click(function(){
+  $('html, body').animate({
+      scrollTop: $('.contactus').offset().top
+  }, 500);
 });
